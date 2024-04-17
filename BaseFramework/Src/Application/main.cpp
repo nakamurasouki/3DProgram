126d2293662bf7078270a3ae1c5200b0fcd94e49
nakamurasouki <kd1342317@st.kobedenshi.ac.jp>
2024-04-17 10:30:53 +0900

    確認用テスト
    絶対消す

--- BaseFramework/Src/Application/main.cpp
+++ BaseFramework/Src/Application/main.cpp
@@ -221,6 +221,9 @@ bool Application::Init(int w, int h)
 	//===================================================================
 	KdAudioManager::Instance().Init();
 
+	//テスト ※絶対消す※
+	m_test = 10;
+
 	return true;
 }
 
